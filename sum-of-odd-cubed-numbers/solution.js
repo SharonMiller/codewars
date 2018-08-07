98291f320e291f57ca60b29232690a3445e14886
Sharon Miller <sharon@sharonmillercreative.com>
2018-08-06 22:38:42 -0700

    refactor code

--- sum-of-odd-cubed-numbers/solution.js
+++ sum-of-odd-cubed-numbers/solution.js
@@ -1,9 +1,2 @@
 'use-strict'
-function cubeOdd(arr) {
-  if (arr.some(isNaN)) {
-    return undefined
-  }
-  else {
-    return arr.filter((num) => num % 2 !== 0).map((num) => num * num * num).reduce((a, b) => a + b, 0)
-  }
-}
+cubeOdd=(arr)=>(arr.some(isNaN))?undefined:arr.filter((num) => num % 2 !== 0).map((num) => num * num * num).reduce((a, b) => a + b, 0)
